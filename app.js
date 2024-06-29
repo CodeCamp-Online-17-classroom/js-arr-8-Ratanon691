4ba2505c281d91a53f42acd6913c2dba194603e1
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-29 23:38:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+function lessThan0(arr) {
+for (i = 0 ; i < arr.length ; i++){
+  if(arr[i] < 0){
+    console.log(arr[i])
+    break
+  }
+}
+}
+const nums = [7, 9, -5, -1, 0, 3];
+// expexted result: -5
